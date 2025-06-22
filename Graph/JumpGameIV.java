c0fc2c9a4c21aa986706f8cec8aa86806dec6733
shivam3246 <shivam3246@gmail.com>
2025-06-23 02:13:27 +0530

    "23/06"

---
+++ Graph/JumpGameIV.java
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/jump-game-iv/description/
+package Graph;
+
+public class JumpGameIV {
+    static int bfs(int []arr){
+        int n = arr.length;
+        if(n<=1){
+            return 0; //this gives the answer if we have only 1 element we are already at destination
+        }
+    }
+    public static void main(String[] args) {
+       int []arr = {100,-23,-23,404,100,23,23,23,3,404};
+
+
+    }
+}
